5d019e99ee9ebb028aaabe2def5ef0c4bc833acc
Daria Vasyukova <daria.voznyak@gmail.com>
2022-12-21 00:08:11 +0300

    Day 3 stub

---
+++ src/day3.py
@@ -0,0 +1,18 @@
+# Day 3: Rucksack Reorganization
+# Problem statement: https://adventofcode.com/2022/day/3
+
+
+def part1(text_input):
+    return 0
+
+
+def part2(text_input):
+    return 0
+
+
+def run(input_path):
+    print("Day  3: Rucksack Reorganization")
+    with open(input_path) as f:
+        content = f.read()
+    print("Part 1:", part1(content))
+    print("Part 2:", part2(content))
