51f04ec16caaa9cb035e6b65a69f680f0bc5ed66
Daria Vasyukova <daria.voznyak@gmail.com>
2022-12-21 23:42:58 +0300

    Day 5 - supply stacks

---
+++ src/day5.py
@@ -0,0 +1,49 @@
+# Day 5: Supply Stacks
+# Problem statement: https://adventofcode.com/2022/day/5
+
+from collections import namedtuple
+
+
+def parse_stacks(stacks):
+    lines = stacks.split("\n")
+    num_stacks = int(lines[-1].split()[-1])
+    stacks = [[] for _ in range(num_stacks)]
+    for line in reversed(lines[:-1]):
+        for i in range(num_stacks):
+            crate = line[1 + 4 * i]
+            if crate != " ":
+                stacks[i].append(crate)
+    return stacks
+
+
+def parse_moves(moves):
+    for line in moves.split("\n"):
+        num, from_stack, to_stack = map(int, line.split()[1::2])
+        yield (num, from_stack - 1, to_stack - 1)
+
+
+def part1(text_input):
+    stacks, moves = text_input.split("\n\n")
+    stacks = parse_stacks(stacks)
+    for num, from_stack, to_stack in parse_moves(moves):
+        for _ in range(num):
+            elem = stacks[from_stack].pop()
+            stacks[to_stack].append(elem)
+    return "".join(stack[-1] for stack in stacks)
+
+
+def part2(text_input):
+    stacks, moves = text_input.split("\n\n")
+    stacks = parse_stacks(stacks)
+    for num, from_stack, to_stack in parse_moves(moves):
+        elems = [stacks[from_stack].pop() for _ in range(num)]
+        stacks[to_stack].extend(reversed(elems))
+    return "".join(stack[-1] for stack in stacks)
+
+
+def run(input_path):
+    print("Day  5: Supply Stacks")
+    with open(input_path) as f:
+        content = f.read()
+    print("Part 1:", part1(content))
+    print("Part 2:", part2(content))
